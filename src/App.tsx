5ed10c0c440cf7983e02f7594fe45668effdd732
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-03-19 06:42:12 -0500

    Update BrowserRouter basename in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import "./App.css"
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<Index />}></Route>
         <Route
